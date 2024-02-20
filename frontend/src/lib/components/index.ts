65f2a90554eb0dbbfa8905f5e2e17c5b84a4f16f
Robin Löffel <hi@robinloeffel.ch>
2024-02-20 19:04:37 +0100

    fix: remove old export

--- frontend/src/lib/components/index.ts
+++ frontend/src/lib/components/index.ts
@@ -1,5 +1,4 @@
 export { default as PageHero } from "./page-hero.svelte";
 export { default as Navigation } from "./navigation.svelte";
 export { default as ContentArea } from "./content-area.svelte";
-export { default as Spacer } from "./spacer.svelte";
 export { default as Richtext } from "./richtext.svelte";
