3f37a85b11fc71a09aa9fe4ce6deca174f521266
Robin Löffel <hi@robinloeffel.ch>
2024-02-23 15:08:09 +0100

    chore: update deps

--- frontend/sanity-codegen.config.ts
+++ frontend/sanity-codegen.config.ts
@@ -1,8 +1,6 @@
 import type { SanityCodegenConfig } from "@sanity-codegen/cli";
 
-const config: SanityCodegenConfig = {
+export default {
 	sanityConfigPath: "../backend/sanity.config.ts",
 	output: "./src/lib/sanity/types.d.ts"
-};
-
-export default config;
+} satisfies SanityCodegenConfig;
